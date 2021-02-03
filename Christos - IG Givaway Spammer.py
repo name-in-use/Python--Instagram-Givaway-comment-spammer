54b25f1ba4b46e6c15446c9c234f3964a4bf89d5
name-in-use <61110698+name-in-use@users.noreply.github.com>
2021-02-03 03:19:21 -1100

    Update Christos - IG Givaway Spammer.py

--- Christos - IG Givaway Spammer.py
+++ Christos - IG Givaway Spammer.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 import os
-from rp import randompass
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
